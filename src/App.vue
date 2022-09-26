49200b9f62ff84b964cf535b8d899c3c906c9a8b
boski <boskitechh@gmail.com>
2022-09-27 00:36:06 +0100

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue';
+
+</script>
+
+<template>
+  <h1 class="text-center">Hello People</h1>
+  <HelloWorld />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1 class="text-5xl">Yoo People</h1>
+</template>
+
+<style scoped>
+
+</style>
