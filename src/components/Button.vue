cf5b3205b36d5833b2275df91c4760e4c270306a
boski <boskitechh@gmail.com>
2022-09-28 15:35:13 +0100

    Added Keypress events

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-    <div @click="textClick" class="disable-text-select h-6 w-5 md:w-8 md:h-8 lg:w-14 lg:h-14 xl:w-16 xl:h-16 shrink flex justify-center items-center rounded-md lg:rounded-lg shadow-sm bg-gray-50 hover:bg-gray-200 hover:cursor-pointer" @click.right.prevent>
+    <div @click="textClick" class="disable-text-select h-6 w-5 md:w-8 md:h-8 lg:w-14 lg:h-14 xl:w-16 xl:h-16 shrink flex justify-center items-center rounded-md lg:rounded-lg shadow-sm bg-gray-50 hover:bg-gray-200 hover:cursor-pointer" :class="{ 'bg-gray-400': toggleButton}" @click.right.prevent>
         <div class="text-gray-900 text-sm lg:text-2xl">    
             <div v-if="shiftValue == false">
                 {{value.lowercase}}
@@ -20,6 +20,7 @@ export default {
     data(){
         return{
             shiftValue: this.checkShift,
+            toggleButton: false
         }
     },
     methods:{
@@ -29,14 +30,28 @@ export default {
             }else{
                 this.$emit('input-text', this.value.lowercase)
             }
-            
         },
+        doCommand(e) {
+            let cmd = String.fromCharCode(e.keyCode).toLowerCase();
+            if ((this.shiftValue == false && cmd === this.value.lowercase) || ((this.shiftValue == true) && (cmd === this.value.uppercase || cmd === this.value.lowercase))){
+                this.toggleButton = true
+                setTimeout(() => {
+                    this.toggleButton = false
+                }, 150)
+            }
+        }
     },
     watch: {
         checkShift: function (newVal) {
             this.shiftValue = newVal
         }
-    }
+    },
+    created() {
+        window.addEventListener('keypress', this.doCommand);
+    },
+    destroyed() {
+        window.removeEventListener('keypress', this.doCommand);
+    },
 }
 </script>
 
--- src/components/GrowButton.vue
+++ src/components/GrowButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <div @click="textClick"  class="disable-text-select grow md:h-8 lg:h-14 xl:h-16 flex justify-center items-center rounded lg:rounded-lg shadow-sm bg-gray-50 hover:bg-gray-200 hover:cursor-pointer" @click.right.prevent>
+    <div @click="textClick"  class="disable-text-select grow md:h-8 lg:h-14 xl:h-16 flex justify-center items-center rounded lg:rounded-lg shadow-sm bg-gray-50 hover:bg-gray-200 hover:cursor-pointer" :class="{ 'bg-gray-400': toggleButton}" @click.right.prevent>
         <div class="text-gray-900 text-sm lg:text-xl">
             {{value}}
         </div>
@@ -9,13 +9,38 @@
 <script>
 export default {
     props: {
-        value: String
+        value: String,
+    },
+    data() {
+        return {
+            toggleButton: false
+        }
     },
     methods: {
         textClick() {
             this.$emit('input-text', this.value)
+        },
+        doCommand(e) {
+            let cmd = String.fromCharCode(e.keyCode).toLowerCase();
+            console.log(e.keyCode)
+            
+            if (cmd === this.value) {
+                this.toggleButton = true
+                setTimeout(() => {
+                    this.toggleButton = false
+
+                }, 150)
+            }
         }
-    }
+
+    },
+    created() {
+        window.addEventListener('keypress', this.doCommand);
+    },
+    destroyed() {
+        window.removeEventListener('keypress', this.doCommand);
+    },
+
 }
 </script>
 
