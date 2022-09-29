2df195cc5493fea4a721ae0ae11e5f0927f45cf8
boski <boskitechh@gmail.com>
2022-09-29 02:12:07 +0100

    Finishing changes

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,8 +1,8 @@
 <template>
     <div 
         @click="textClick" 
-        class="disable-text-select h-7 w-5 md:w-8 md:h-8 lg:w-14 lg:h-14 xl:w-16 xl:h-16 shrink flex justify-center items-center rounded-md lg:rounded-lg shadow-md bg-gray-50 hover:cursor-pointer" 
-        :class="{ 'bg-gray-300': toggleButton, 'bg-slate-200': clickEffect}" 
+        class="disable-text-select h-7 w-5 md:w-8 md:h-8 lg:w-14 lg:h-14 xl:w-16 xl:h-16 shrink flex justify-center items-center rounded-md lg:rounded-lg shadow-md hover:bg-gray-100 bg-gray-50 hover:cursor-pointer" 
+        :class="{ 'bg-gray-300': toggleButton, 'bg-slate-300 hover:bg-slate-300': clickEffect}" 
         @click.right.prevent
     >
         <div class="text-gray-900 text-sm lg:text-2xl">    
--- src/components/GrowButton.vue
+++ src/components/GrowButton.vue
@@ -1,8 +1,8 @@
 <template>
     <div 
         @click="textClick"  
-        class="relative disable-text-select grow md:h-8 lg:h-14 xl:h-16 flex justify-center items-center rounded lg:rounded-lg shadow-md bg-gray-50 hover:cursor-pointer" 
-        :class="{'bg-gray-300': toggleButton, 'bg-slate-200': clickEffect}" 
+        class="relative disable-text-select grow md:h-8 lg:h-14 xl:h-16 flex justify-center items-center rounded lg:rounded-lg shadow-md hover:bg-gray-100 bg-gray-50 hover:cursor-pointer" 
+        :class="{'bg-gray-300': toggleButton, 'bg-slate-200 hover:bg-slate-200': clickEffect}" 
         @click.right.prevent
     >
         <div class="text-gray-900 text-sm lg:text-xl">
